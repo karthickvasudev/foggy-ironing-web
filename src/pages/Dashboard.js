da9cf49633b27c02ee3d0891220450ca67f57ec8
pradeeptungsten <pradeep.tungsten@gmail.com>
2022-10-16 09:33:53 -0400

    dashboard tile completed 16-010

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,19 +4,74 @@ import { Navigate, useNavigate } from "react-router-dom";
 const constants = require("../constants/Constants");
 
 export default function Dashboard() {
-  const navigate = useNavigate();
-
-  const logoutSuccess = () => {
-    sessionStorage.clear();
-    navigate("/", { replace: true });
-  };
+  const values={order:1,completed:1,revenue:1001+".00"}
   return (
-    <div>
-      <GoogleLogout
-        clientId={constants.CLIENT_ID}
-        buttonText="Logout"
-        onLogoutSuccess={logoutSuccess}
-      ></GoogleLogout>
+    <>
+      {bigscreen(values)}
+      {smallscreen(values)}
+    </>
+  );
+}
+
+function bigscreen(props) {
+  const {order,completed,revenue} = props;
+  return (
+    <div className="d-none d-md-block">
+      <div className="row justify-content-center ">
+        <span className="col-3 ">
+          <div className="card bg-success ">
+            <h5 className="card-title m-2">Order</h5>
+            <div className="card-body">
+              <p class="fs-1 text-center mb-0">{order}</p>
+            </div>
+          </div>
+        </span>
+        <span className="col-3 ">
+          <div className="card bg-secondary">
+          <h5 className="card-title m-2">Completed</h5>
+            <div className="card-body">
+              
+              <p class="fs-1 text-center mb-0">{completed}</p>
+            </div>
+          </div>
+        </span>
+        <span className="col-3 ">
+          <div className="card bg-success">
+          <h5 className="card-title m-2">Revenue </h5>  
+            <div className="card-body ">
+              
+              <p class="fs-1 text-center mb-0">₹ {revenue}</p>
+            </div>
+          </div>
+        </span>
+      </div>
     </div>
+  );
+}
+function smallscreen(props) {
+  const {order,completed,revenue} = props;
+  return (
+    <span className="card-body text-center">
+      <h5 className="card-title row bg-success d-flex justify-content-center d-md-none m-3 pt-3 rounded-4">
+        <span className="col-4">
+          Order
+          <div>
+            <p className="fs-1 text-center mt-2">{order}</p>
+          </div>
+        </span>
+        <span className="col-4">
+          Completed
+          <div>
+            <p className="fs-1 text-center mb-0 mt-2">{completed}</p>
+          </div>
+        </span>
+        <span className="col-4">
+          Revenue
+          <div>
+            <p className="fs-1 text-center mb-0 mt-2">₹ {revenue}</p>
+          </div>
+        </span>
+      </h5>
+    </span>
   );
 }

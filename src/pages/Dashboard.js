60a3296463cb263db092a98dd15284b1af5f5e11
pradeeptungsten <pradeep.tungsten@gmail.com>
2022-10-21 11:43:28 -0400

    create oder completed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,44 +1,48 @@
 import React from "react";
+import LaptopTables from "../components/LaptopTables";
 const constants = require("../constants/Constants");
 
 export default function Dashboard() {
-  const values={order:1,completed:1,revenue:1001+".00"}
+  const values = { order: 1, completed: 1, revenue: 1001 + ".00" };
   return (
     <>
-      {bigscreen(values)}
-      {smallscreen(values)}
+      {BigScreen(values)}
+      {SmallScreen(values)}
+      {dashboardList()}
+
+      {ListDetails()}
     </>
   );
 }
 
-function bigscreen(props) {
-  const {order,completed,revenue} = props;
+function BigScreen(props) {
+  const { order, completed, revenue } = props;
   return (
     <div className="d-none d-md-block">
       <div className="row justify-content-center ">
         <span className="col-3 ">
-          <div className="card bg-success ">
+          <div className="card ">
             <h5 className="card-title m-2">Order</h5>
             <div className="card-body">
-              <p class="fs-1 text-center mb-0">{order}</p>
+              <p class="fs-3 text-center mb-0">{order}</p>
             </div>
           </div>
         </span>
-        <span className="col-3 ">
-          <div className="card bg-secondary">
-          <h5 className="card-title m-2">Completed</h5>
+        <span className="col-3">
+          <div className="card">
+            <h5 className="card-title m-2">Completed</h5>
+           
+  
+   <div className="card-body">
+              <p class="fs-3 text-center mb-0">{completed}</p>
+            </div>
+          </div>
+        </span>
+        <span className="col-3">
+          <div className="card">
+            <h5 className="card-title m-2">Revenue</h5>
             <div className="card-body">
-
-              <p class="fs-1 text-center mb-0">{completed}</p>
-            </div>
-          </div>
-        </span>
-        <span className="col-3 ">
-          <div className="card bg-success">
-          <h5 className="card-title m-2">Revenue </h5>
-            <div className="card-body ">
-
-              <p class="fs-1 text-center mb-0">₹ {revenue}</p>
+              <p class="fs-3 text-center mb-0">₹ {revenue}</p>
             </div>
           </div>
         </span>
@@ -46,30 +50,64 @@ function bigscreen(props) {
     </div>
   );
 }
-function smallscreen(props) {
-  const {order,completed,revenue} = props;
+function SmallScreen(props) {
+  const { order, completed, revenue } = props;
   return (
-    <span className="card-body text-center">
-      <h5 className="card-title row bg-success d-flex justify-content-center d-md-none m-3 pt-3 rounded-4">
-        <span className="col-4">
-          Order
-          <div>
-            <p className="fs-1 text-center mt-2">{order}</p>
-          </div>
-        </span>
-        <span className="col-4">
-          Completed
-          <div>
-            <p className="fs-1 text-center mb-0 mt-2">{completed}</p>
-          </div>
-        </span>
-        <span className="col-4">
-          Revenue
-          <div>
-            <p className="fs-1 text-center mb-0 mt-2">₹ {revenue}</p>
-          </div>
-        </span>
-      </h5>
-    </span>
+    <div className="card d-flex justify-content-center d-md-none m-3 pt-3 rounded-4">
+      <span className="card-body text-center">
+        <h5 className="card-title row">
+          <span className="col-4">
+            Order
+            <div>
+              <p className="fs-3 text-center mt-2">{order}</p>
+            </div>
+          </span>
+          <span className="col-4">
+            Completed
+            <div>
+              <p className="fs-3   text-center mb-0 mt-2">{completed}</p>
+            </div>
+          </span>
+          <span className="col-4">
+            Revenue
+            <div>
+              <p className="fs-3 text-center mb-0 mt-2">₹ {revenue}</p>
+            </div>
+          </span>
+        </h5>
+      </span>
+    </div>
   );
+}
+function dashboardList() {
+  return (
+    <div className="d-flex justify-content-center mt-5 ">
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <span class="nav-link active px-4" aria-current="page" href="#">
+            New Orders
+          </span>
+        </li>
+        <li class="nav-item">
+          <span class="nav-link  px-4" href="#">
+            Completed Orders
+          </span>
+        </li>
+        <li class="nav-item">
+          <span class="nav-link px-4">Delivered Orders</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function ListDetails() {
+
+ if (true) {
+    return (
+      <div className="mt-3">
+        <h1>new orders</h1>
+      </div>
+    );
+  }
 }
